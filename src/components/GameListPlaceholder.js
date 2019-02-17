8aaedc4da481c78140f68e77f0af76ce4d65bafa
Mark Nash <abacefabmicroblast@hotmail.com>
2019-02-17 06:12:23 -0500

    Sriv's population of events from firebase

--- src/components/GameListPlaceholder.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class GameListPlaceholder extends Component {
-  render() {
-    return (
-      <h2>This is the game list placeholder component</h2>
-    )
-  }
-}
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import {Route} from "react-router-dom";
 
 import LoginOptions from "./login-options/loginOptions"
-import GameListPlaceholder from "./GameListPlaceholder";
 import CreateGame from "./create-game/CreateGame";
 import GetUser from "./get-user/GetUser";
 import GameOptions from "./game-options/GameOptions";
 import GameInfoPlaceholder from "./GameInfoPlaceholder";
+import JoinGroup from "./join-group/JoinGroup";
 
 export default class Routes extends Component {
 
@@ -16,7 +16,7 @@ export default class Routes extends Component {
         <Route exact path="/" component={LoginOptions}/>
         <Route path="/login-signup:key" component={Child}/>
         <Route path="/game-options" component={GameOptions}/>
-        <Route path="/game-list" component={GameListPlaceholder}/>
+        <Route path="/game-list" component={JoinGroup}/>
         <Route path="/create-game" component={CreateGame}/>
         <Route path="/game-info" component={GameInfoPlaceholder}/>
       </div>
