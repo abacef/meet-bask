8d6816bd6fb111ef81ad894d40bae46024d3356b
Mark Nash <abacefabmicroblast@hotmail.com>
2019-02-17 00:02:46 -0500

    merged dBarrous changes

--- src/components/CreateUserPlaceholder.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class CreateUserPlaceholder extends Component {
-  render() {
-    return (
-      <h2>This is the create user placeholder component</h2>
-    )
-  }
-}
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import {Route} from "react-router-dom";
 
 import LoginOptions from "./loginOptions/loginOptions"
-import CreateUserPlaceholder from "./CreateUserPlaceholder";
 import LoginPlaceholder from "./LoginPlaceholder";
 import GameListPlaceholder from "./GameListPlaceholder";
 import CreateGameFolder from "./CreateGameFolder/CreateGameFolder";
+import CreateUserFolder from "./CreateUserFolder/CreateUserFolder";
 
 export default class Routes extends Component {
 
@@ -13,7 +13,7 @@ export default class Routes extends Component {
     return (
       <div>
         <Route exact path="/" component={LoginOptions}/>
-        <Route exact path="/new-user" component={CreateUserPlaceholder}/>
+        <Route exact path="/new-user" component={CreateUserFolder}/>
         <Route exact path="/login" component={LoginPlaceholder}/>
         <Route exact path="/game-list" component={GameListPlaceholder}/>
         <Route exact path="/create-game" component={CreateGameFolder}/>
